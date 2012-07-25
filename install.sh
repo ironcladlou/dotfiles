fa8a608b2fc59d25c6c6f3be004aea3e3d84b9cb
Dan Mace <ironcladlou@gmail.com>
2012-07-25 13:38:57 -0400

    Improve installer and test churner

--- install.sh
+++ install.sh
@@ -13,13 +13,14 @@ df_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "Installing from dotfiles dir ${df_dir}"
 
-for f in $(find "${df_dir}/bin" -type f -maxdepth 1); do
+for f in $(find "${df_dir}/bin" -maxdepth 1 -type f); do
 	target="${HOME}/bin/$(basename ${f})"
 
 	if [ -f "${target}" ]; then
-		echo "${target} is already installed, skipping"
-	else
-		echo "Installing ${f} => ${target}"
-		ln -s $f $target
+		echo "${target} is already installed, removing for update"
+    rm $target
 	fi
+	
+  echo "Installing ${f} => ${target}"
+	ln -s $f $target
 done
