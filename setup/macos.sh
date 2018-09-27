1088f873a4ce72c32ff033a8c2817dadf1574895
Dan Mace <ironcladlou@gmail.com>
2018-09-27 19:42:17 -0400

    Various changes

--- setup/macos.sh
+++ setup/macos.sh
@@ -14,7 +14,7 @@ function dotfiles::install_go {
 
   echo "installing golang $version"
   local tarball="go${version}.darwin-amd64.tar.gz"
-  wget "https://storage.googleapis.com/golang/${tarball}"
+  wget "https://dl.google.com/go/${tarball}"
   tar zxf $tarball
   mv go $HOME/.go/$version
   rm -f $tarball
@@ -22,3 +22,8 @@ function dotfiles::install_go {
 
 dotfiles::install_go 1.8.3
 dotfiles::install_go 1.9
+dotfiles::install_go 1.10.3
+dotfiles::install_go 1.10.4
+dotfiles::install_go 1.11
+
+#~/Library/Application Support/Code/User/{keybindings,settings}.json
